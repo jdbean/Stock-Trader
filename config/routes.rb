dcc395d6b2e8af49519469ec61b585d2cd2678cd
J.D. Bean <jonathandbean@gmail.com>
2018-11-21 14:30:09 -0500

    Minor config tweaks (routing and js requires)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'portfolios#index'
+  # resources :portfolios, only: [:index], 
+  get "portfolio" => "portfolios#index"
   resources :transactions, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
