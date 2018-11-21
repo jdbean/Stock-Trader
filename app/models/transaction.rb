1f753034f6e650d1b99ab834c12f2b808952e434
J.D. Bean <jonathandbean@gmail.com>
2018-11-21 08:58:07 -0500

    Syntax error in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
-  belongs_to: :user
+  belongs_to :user
 
   validates :symbol, presence: true, length: { in: 1..5 }
   validates :quantity,
@@ -8,5 +8,5 @@ class Transaction < ApplicationRecord
       only_integer: true,
       greater_than: 0
     }
-  validates :price_per_share, presence: true, numericality: { greater_than: 0 }
+  validates :share_price, presence: true, numericality: { greater_than: 0 }
 end
