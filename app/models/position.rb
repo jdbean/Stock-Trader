5359d85e2f94c0dfbab937be1de5229cafd5336a
J.D. Bean <jonathandbean@gmail.com>
2018-11-24 13:04:34 -0500

    Remove positions model. Sticking with calculating positions totals for now

--- app/models/position.rb
+++
@@ -1,9 +0,0 @@
-class Position < ApplicationRecord
-  validates :symbol, presence: true, length: { in: 1..5 }
-  validates :num_shares,
-     presence: true, 
-     numericality: {
-      only_integer: true,
-      greater_than: 0
-    }
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_162158) do
+ActiveRecord::Schema.define(version: 2018_11_20_162012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "positions", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "symbol", null: false
-    t.integer "quantity", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_owned_shares_on_user_id"
-  end
 
   create_table "transactions", force: :cascade do |t|
     t.string "symbol", null: false
