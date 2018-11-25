f6ce535fed6a0a3e09f21eef7884e82cbfdf79c5
J.D. Bean <jonathandbean@gmail.com>
2018-11-24 19:55:41 -0500

    Customize devise registration controller to add name field and validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :get_stock_price
 
@@ -31,4 +32,8 @@ class ApplicationController < ActionController::Base
       conn.adapter :typhoeus
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+ end
 end
