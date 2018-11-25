914c5222df93be7a371083ced742061fcc4b1257
J.D. Bean <jonathandbean@gmail.com>
2018-11-25 11:57:20 -0500

    Working around order/search bug for content_tag based fields in portfolio table.

--- app/datatables/portfolios_datatable.rb
+++ app/datatables/portfolios_datatable.rb
@@ -1,11 +1,12 @@
 class PortfoliosDatatable < Effective::Datatable
   datatable do
+    order :current_value, :desc
     length 5  # 5, 10, 25, 50, 100, 500, :all
 
-    col :symbol, as: :string,
-        # search: { as: :select, collection: current_user.transactions.select(:symbol).distinct.pluck(:symbol), include_null: false  },
-        search: { fuzzy: true },
-        responsive: "1" do |sym|
+    val :symbol, as: :string, search: { fuzzy: true }, responsive: "1" do |holding|
+      holding[0]
+    end.format do |sym|
+      # search: { as: :select, collection: current_user.transactions.select(:symbol).distinct.pluck(:symbol), include_null: false  },
       case attributes[:status_hash][sym]
       when -1
         content_tag(:span, sym, class: "red")
@@ -15,11 +16,15 @@ class PortfoliosDatatable < Effective::Datatable
         content_tag(:span, sym, class: "gray")
       end
     end
+
     col :shares, as: :integer, responsive: "4"
     col :openening_price, as: :currency, responsive: "5"
     col :latest_price, as: :currency, responsive: "5"
-    col :daily_performance, as: :percent, responsive: "3" do |perf|
-      percent = number_to_percentage(perf)
+
+    val :daily_performance, responsive: "3" do |holding|
+      holding[4]
+    end.format do |perf|
+      percent = number_to_percentage(perf, precison: 2)
       case
       when perf < 0.0
         content_tag(:span, percent, class: "red")
@@ -29,11 +34,15 @@ class PortfoliosDatatable < Effective::Datatable
         content_tag(:span, percent, class: "gray")
       end
     end
-    val :current_value,  as: :currency, sort: true, responsive: "2", search: { fuzzy: true } do |holding|
+
+    val :current_value, responsive: "2", search: { fuzzy: true } do |holding|
       sym = holding[0]
       value = (holding[1] * holding[3])
       @total_value += value
-      value = number_to_currency(value)
+      value
+    end.format do |val, holding|
+      sym = holding[0].match(/>(.*)</)[1]
+      value = number_to_currency(val)
       case attributes[:status_hash][sym]
       when -1
         content_tag(:span, value, class: "red")
@@ -43,10 +52,11 @@ class PortfoliosDatatable < Effective::Datatable
         content_tag(:span, value, class: "gray")
       end
     end
+
     aggregate :total, label: "Total Portfolio Value" do |values, column|
       if column[:name] == :symbol
         "Portfolio Value"
-      elsif column[:name] == :current_value 
+      elsif column[:name] == :current_value
         content_tag(:span, number_to_currency(@total_value))
       end
     end
